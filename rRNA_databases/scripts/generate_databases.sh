f572ed96af270cf236a034dfe103ecdf3a58b4e3
John <jmeppley@gmail.com>
2019-03-01 23:37:44 +0000

    updaating script for silva 132 pipeline

--- rRNA_databases/scripts/generate_databases.sh
+++ rRNA_databases/scripts/generate_databases.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-rootdir="/home/evko1434/ARB_SILVA_databases"
-reference_db=(bacteria_16S_119_clean-T.fasta archaea_16S_119_clean-T.fasta eukarya_18S_119_clean-T.fasta bacteria_23S_clean-T.fasta archaea_23S_clean-T.fasta eukarya_28S_clean-T.fasta RF00001-T.fasta.txt RF00002-T.fasta.txt)
+# usage: run generate_databases.sh on cleaned rRNA databases (in gzipped FASTA format)
+#  to cluster and genrate fasta file of cluster representatives
+#
+# Requirements:
+#   vsearch (v2.10.4)
+
+
+rootdir=/home/jmeppley/lus/sortmerna/nexus
+indir=${rootdir}/fasta-clean
+outdir=${rootdir}/fasta-final
+mkdir -p ${outdir}
+
+vsearch_threads=20
+
+reference_db=(bacteria_16S_132 archaea_16S_132 eukarya_18S_132 bacteria_23S_132 archaea_23S_132 eukarya_28S_132 RF00001 RF00002)
 id=(0.90 0.95 0.95 0.98 0.98 0.98 0.98 0.98)
 
-#for i in 0;
 for ((i=0;i<${#reference_db[@]};i++));
 do
-    echo "sumaclust -l -p 40 -t ${id[i]} -F $rootdir/step4-c/${reference_db[i]} $rootdir/${reference_db[i]}"
-    echo "sumaclust -l -p 40 -t ${id[i]} -F $rootdir/step4-c/${reference_db[i]} $rootdir/${reference_db[i]}" | qsub -l nodes=1:ppn=40 -q mem4gbq -k oe -N ${db}_sumaclust; sleep 2
+    db=${reference_db[i]}
+    fasta_gz=${indir}/${db}_clean.fasta.gz
+    reps_fasta=${outdir}/${db}.id${id[i]}.fasta
+    cmd="vsearch --cluster_fast ${fasta_gz} --gzip_decompress \
+                 --id ${id[i]} --iddef 2 --strand plus \
+                 --no_progress --threads ${vsearch_threads} \
+                 --centroids ${reps_fasta}"
+    echo ${cmd}
+    `$cmd`
+
+    #echo "sumaclust -l -p 40 -t ${id[i]} -F $rootdir/step4-c/${reference_db[i]} $rootdir/${reference_db[i]}" | qsub -l nodes=1:ppn=40 -q mem4gbq -k oe -N ${db}_sumaclust; sleep 2
 done
